7bedd1ee1ac45a6a6616043a020b1c43ee2c5ca8
João Albergaria <jvalbergariabar@gmail.com>
2026-03-08 16:56:21 -0300

    Atualização dos Termos e Condições
    <ul><li> não coloca os pontinhos. Revisar isso

--- app/lib/termsAndConditions.tsx
+++ app/lib/termsAndConditions.tsx
@@ -5,90 +5,78 @@ const TermsAndConditions = () => {
   return (
     <div className='termsAndConditions'>
       
-      <b>TERMOS E CONDIÇÕES DE PARTICIPAÇÃO NO FAMUN {year}</b><br />
+      <b>INSCRIÇÃO</b><br />
+      Antes de dar início à inscrição de sua delegação no FAMUN {year}, é necessário que o Chefe de Delegação ou Professor(a) Orientador(a) crie um login e uma senha no sistema de inscrições.
+      <br />
+      <br />
 
+      <b>TERMOS E CONDIÇÕES DE PARTICIPAÇÃO NO FAMUN {year}</b><br />
       Delegados(as) e Professores(as) Orientadores(as) reconhecem e declaram estar de acordo com os seguintes termos e condições de participação no FAMUN {year}:
       <br />
       <br />
 
       <b>Inscrições</b><br />
       Ensino Médio: para garantir a maior diversidade de participantes no FAMUN {year}, cada escola poderá inscrever uma única delegação de, no máximo, 10 alunos(as).
-
       <br />
       Universidade: cada universidade poderá inscrever delegações de, no máximo, 10 alunos(as). Não há limite para o número de delegações para universidade.
-
       <br />
-      As vagas são limitadas e, por isso, as inscrições serão confirmadas por ordem de pagamento da inscrição. Caso as vagas se encerrem, a equipe organizadora do FAMUN {year} abrirá uma lista de espera. As escolas em lista de espera terão suas inscrições confirmadas apenas se houver desistência de outras delegações.
+      As vagas são limitadas e, por isso, as inscrições serão confirmadas por ordem de <b>pagamento da inscrição</b>. Caso as vagas se encerrem, a equipe organizadora do FAMUN {year} abrirá uma lista de espera. As escolas em lista de espera terão suas inscrições confirmadas apenas se houver desistência de outras delegações.
       <br />
-      As escolas em lista de espera terão sua inscrição confirmada apenas se houver desistência de outras delegações.
-      <br />
-      Caso necessário, a equipe organizadora do FAMUN {year} poderá enviar uma carta convite para confirmar a participação da escola/universidade na conferência.
+      Caso necessário, a equipe organizadora do FAMUN {year} poderá enviar uma carta-convite para confirmar a participação da escola/universidade na conferência.
       <br />
       <br />
 
       <b>Pagamentos</b><br />
-      Todos os pagamentos deverão ser feitos em até 7 dias corridos após a submissão da inscrição. Caso os pagamentos não sejam realizados nesse período, a inscrição será cancelada e a escola/universidade deverá realizar uma nova inscrição, caso deseje participar da conferência. A nova inscrição dependerá da disponibilidade de vagas.
-
+      Todos os pagamentos deverão ser feitos em até <b>7 dias corridos</b> após a submissão da inscrição. Caso os pagamentos não sejam realizados nesse período, a inscrição será cancelada e a escola/universidade deverá realizar uma nova inscrição, caso deseje participar da conferência. A nova inscrição dependerá da disponibilidade de vagas.
       <br />
-
       Os pagamentos nacionais devem ser feitos via PIX, cartão de crédito ou boleto. Pagamentos internacionais devem ser feitos via cartão de crédito.
       <br />
-
       O FAMUN {year} não realiza reembolso ou ressarcimento das taxas de inscrição, sob nenhuma circunstância.
       <br />
       <br />
 
       <b>Inserção dos dados da delegação</b><br />
-      Após a confirmação do pagamento da inscrição, a delegação terá até a data limite de 02 de junho de {year} para preencher os dados pessoais de todos(as) os(as) delegados(as) e Professores(as) Orientadores(as) que comporão a delegação no sistema de inscrições.
+      Após a confirmação do pagamento da inscrição, a delegação terá <b>até a data limite de 12 de junho de {year}</b> para preencher os dados pessoais de todos(as) os(as) delegados(as) e Professores(as) Orientadores(as) que comporão a delegação no sistema de inscrições.
       <br />
-      ATENÇÃO: Os dados devem ser preenchidos necessariamente até dia 02 de junho de {year}. Caso os dados pessoais de todos(as) os(as) integrantes da delegação não estejam preenchidos, as vagas não preenchidas serão canceladas e transferidas para a lista de espera. As vagas canceladas não terão reembolso.
+      <b>ATENÇÃO: Os dados devem ser preenchidos necessariamente até o dia 12 de junho de {year}.</b> Caso os dados pessoais de todos(as) os(as) integrantes da delegação não estejam preenchidos, as vagas não preenchidas serão canceladas e transferidas para a lista de espera. As vagas canceladas não terão reembolso.
+      <br />
+      <br />
 
       <b>Professores Orientadores</b><br />
-      <b>Ensino Médio:</b> para inscrever a delegação de uma escola no FAMUN {year}, é obrigatório inscrever ao menos um(a) Professor(a) Orientador(a). Tal professor(a) deve ser um adulto responsável dentro das seguintes opções: professor(a); pais; adulto legalmente responsável pelos(as) estudantes
+      <b><i>Ensino Médio:</i></b> para inscrever a delegação de uma escola no FAMUN {year}, é obrigatório inscrever ao menos um(a) Professor(a) Orientador(a). Tal professor(a) deve ser um adulto responsável dentro das seguintes opções: professor(a); pais; adulto legalmente responsável pelos(as) estudantes.
       <br />
-      É obrigatória a presença do(a) Professor(a) Orientador(a) em todos os dias de conferência, acompanhando a delegação. A escola deve consentir com a presença do(a) Professor(a) Orientador(a) em todos os dias da conferência por meio da assinatura do Termo de Responsabilidade (Liability Waiver) especial para Faculty Advisors. É permitido um número máximo de 3 (três) Professores(as) Orientadores(as) por delegação de Ensino Médio.
+      É obrigatória a presença do(a) Professor(a) Orientador(a) em todos os dias de conferência, acompanhando a delegação. <b>A escola deve consentir com a presença do(a) Professor(a) Orientador(a) em todos os dias da conferência por meio da assinatura do Termo de Responsabilidade (Liability Waiver) especial para Faculty Advisors.</b> É permitido um número máximo de 3 (três) Professores(as) Orientadores(as) por delegação de Ensino Médio.
       <br />
-      <b>Universidade:</b> a presença de Professores(as) Orientadores(as) é opcional.
+      <b><i>Universidade:</i></b> a presença de Professores(as) Orientadores(as) é opcional.
       <br />
       <br />
 
       <b>Alteração de dados e substituição de delegados(as) e Professores Orientadores</b><br />
-      As delegações poderão solicitar a alteração de dados ou a substituição de participantes já inscritos (delegados(as) e Professores(as) Orientadores(as)) até a data limite de 08 de agosto de {year} no sistema de inscrições. Após essa data, não serão aceitas nenhuma alteração ou substituição.
+      As delegações poderão solicitar a alteração de dados ou a substituição de participantes já inscritos (delegados(as) e Professores(as) Orientadores(as)) <b>até a data limite de 10 de agosto de {year}</b> no sistema de inscrições. Após essa data, não serão aceitas nenhuma alteração ou substituição.
       <br />
       <br />
 
       <b>Autorização e Termo de Responsabilidade (Liability Waiver)</b><br />
-      Para participar do FAMUN {year}, cada participante deverá assinar o documento Autorização e Termo de Responsabilidade (Liability Waiver) e anexá-lo no sistema de inscrições até a data limite de 25 de agosto de {year}. Também é necessário levar a cópia física do termo e entregá-la no momento do credenciamento, no primeiro dia do FAMUN.
+      Para participar do FAMUN {year}, cada participante deverá assinar o documento Autorização e Termo de Responsabilidade (Liability Waiver) e anexá-lo no sistema de inscrições <b>até a data limite de 28 de agosto de {year}</b>. Também é necessário levar a cópia física do termo e entregá-la no momento do credenciamento, no primeiro dia do FAMUN.
       <br />
       Esse documento dá ao FAMUN e à FACAMP o direito de coletar e utilizar as informações submetidas pelos(as) participantes do FAMUN {year}, bem como conteúdo de mídia em formato de fotografia e vídeo durante as atividades da conferência.
       <br />
-      Para estudantes de Ensino Médio, a autorização deverá ser assinada pelos pais ou guardiões legais. Atenção: esse documento deverá ter a assinatura reconhecida 1) ou por assinatura eletrônica autenticada; 2) ou assinatura reconhecida em cartório.
+      Para estudantes de Ensino Médio, a autorização deverá ser assinada pelos pais ou guardiões legais. <b>Atenção: esse documento deverá ter a assinatura reconhecida 1) ou por assinatura eletrônica autenticada; 2) ou assinatura reconhecida em cartório.</b>
       <br />
       Caso o(a) estudante não entregue esse documento com a assinatura reconhecida, não poderá participar de nenhuma atividade do FAMUN {year}.
       <br />
-      Ademais, delegações de Ensino Médio devem apresentar o Termo de Responsabilidade (Liability Waiver) especial para Faculty Advisors, no qual a escola deve consentir com a presença do(a) Professor(a) Orientador(a) em todos os dias da conferência.
+      Ademais, delegações de Ensino Médio devem apresentar o <b>Termo de Responsabilidade (Liability Waiver) especial para Faculty Advisors</b>, no qual a escola deve consentir com a presença do(a) Professor(a) Orientador(a) em todos os dias da conferência.
       <br />
       <br />
 
       <b>Conduta durante as sessões e atividades do FAMUN {year}</b><br />
       A equipe organizadora do FAMUN {year} é responsável por garantir um ambiente diplomático e respeitoso a todos(as) os(as) participantes da conferência. O FAMUN se reserva ao direito de suspender imediatamente os(as) participantes devido à má-conduta, que inclui (mas não se limita) as seguintes ações:
       <ul>
-        <li>
-          Qualquer ação relacionada à conferência que viole os direitos legais dos demais participantes, como racismo, difamação, assédio, ameaça ou qualquer outro comportamento impróprio;
-
-        </li>
-        <li>
-          Consumo ou posse de álcool ou drogas ilícitas; e consumo de cigarro e tabaco em locais não apropriados;
-        </li>
-        <li>
-          Enviar ou compartilhar conteúdo inapropriado ou que possa prejudicar a operação do computador de outro participante, como por exemplo arquivos contendo vírus;
-        </li>
-        <li>
-          Fornecer informações pessoais falsas;
-        </li>
-        <li>
-          Personificar outro participante, o que inclui (mas não se limita) a fazer o login com o nome de outros participantes.
-        </li>
+        <li>Qualquer ação relacionada à conferência que viole os direitos legais dos demais participantes, como racismo, difamação, assédio, ameaça ou qualquer outro comportamento impróprio;</li>
+        <li>Consumo ou posse de álcool ou drogas ilícitas; e consumo de cigarro e tabaco em locais não apropriados;</li>
+        <li>Enviar ou compartilhar conteúdo inapropriado ou que possa prejudicar a operação do computador de outro participante, como por exemplo arquivos contendo vírus;</li>
+        <li>Fornecer informações pessoais falsas;</li>
+        <li>Personificar outro participante, o que inclui (mas não se limita) a fazer o login com o nome de outros participantes.</li>
       </ul>
       <br />
 
@@ -102,8 +90,8 @@ const TermsAndConditions = () => {
       <br />
       <br />
 
-      <b>Plágio</b><br />
-      Os(as) participantes concordam que todos os materiais produzidos para o FAMUN {year} (como discursos, position papers, emendas, dentre outros) serão trabalhos originais. O plágio não será tolerado na FAMUN {year} e, caso a equipe organizadora verifique casos de plágio, o FAMUN se reserva ao direito de suspender a participação do(a) delegado(a), sem reembolso.
+      <b>Plágio e uso de IA</b><br />
+      Os(as) participantes concordam que todos os materiais produzidos para o FAMUN {year} (como discursos, position papers, emendas, dentre outros) serão trabalhos originais. O plágio e o uso de IA sem a devida revisão não serão tolerados no FAMUN {year} e, caso a equipe organizadora verifique tais casos, o FAMUN se reserva ao direito de suspender a participação do(a) delegado(a), sem reembolso.
       <br />
       <br />
 
